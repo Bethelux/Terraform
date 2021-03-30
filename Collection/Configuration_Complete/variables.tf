21ca9ac6772dfe7a2c83f609d244a491fe1e67d3
Bethelux <baccaglioni.m@gmail.com>
2021-03-30 22:18:49 +0200

    Create variables.tf

---
+++ Collection/Configuration_Complete/variables.tf
@@ -0,0 +1,32 @@
+# Region Credentials
+variable "ncloud_ip" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "ami" {}
+variable "key_name" {
+	description = "Name of the SSH keypair to use in AWS."
+}
+variable "admin_password" {
+  description = "Windows Administrator password to login as."
+}
+variable "NameInstance"	{
+	default = "Instance"
+}
+variable "NameVolume"	{
+	default = "Volume"
+}
+variable "size" {
+	default = 8
+}
+variable "instance_number" {
+ 	default = 1
+}
+variable "instance_type" {
+  	default = "t2.micro"
+}
+variable "availability_zone" {
+  	default = "symphony"
+}
+
+
