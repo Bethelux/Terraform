67a8673831f90ba900528de7eba2589afbd1147d
Bethelux <baccaglioni.m@gmail.com>
2021-03-30 22:16:17 +0200

    Create outputs.tf

---
+++ Collection/Creation_Istance_2Volume_ElasticIP_SecurityGroup/outputs.tf
@@ -0,0 +1,17 @@
+/*output "private_ip" {
+  value = "${aws_instance.winrm.private_ip}"
+}
+*/
+output "elastic ip" {
+  value = "${aws_eip.default.public_ip}"
+}
+
+
+output "Name Instance" {
+  value = "${aws_instance.ec2_instance.tags.Name}"
+}
+
+
+
+
+
