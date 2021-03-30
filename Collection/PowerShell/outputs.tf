ff218d74566166c3e196d50eff2696536a092c2c
Bethelux <baccaglioni.m@gmail.com>
2021-03-30 22:14:36 +0200

    Create outputs.tf

---
+++ Collection/PowerShell/outputs.tf
@@ -0,0 +1,14 @@
+/*output "image_id" {
+  value = "${data.aws_ami.default.id}"
+}
+
+output "private_ip" {
+  value = "${aws_instance.winrm.private_ip}"
+}
+output "elastic ip" {
+  value = "${aws_eip.default.public_ip}"
+}
+output "Name Instance" {
+  value = "${var.NameInstance}"
+}
+*/
