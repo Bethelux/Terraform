0b83ea635dd3eeeb8b529b9128f23293230df3a8
Bethelux <baccaglioni.m@gmail.com>
2021-03-30 22:15:04 +0200

    Create variables.tf

---
+++ Collection/Creazione_Instance_With_2_Volume/variables.tf
@@ -0,0 +1,24 @@
+# Region Credentials
+variable "ncloud_ip" {}
+variable "secret_key" {}
+variable "access_key" {}
+variable "ami_image" {}
+variable "NameInstance"	{
+	default = "Instance"
+}
+variable "NameVolume"	{
+	default = "Volume"
+}
+variable "size" {
+	default = 8
+}
+variable "instance_number" {
+  default = 1
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "availability_zone" {
+	default = "symphony"
+}
+
